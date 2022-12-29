90a7189e04c4eb37b32eab786f2b4fe9389cfbaa
Pratik <pratikshinde875@gmail.com>
2022-12-29 08:52:55 +0530

    Update Abstraction Example

--- Abstraction/src/Main.java
+++ Abstraction/src/Main.java
@@ -4,19 +4,24 @@ import Shapes.Rectangle;
 import Shapes.Square;
 import Shapes.Triangle;
 
+/**
+ * This is a Driver class to drive the functionality of
+ * abstraction module.
+ */
 public class Main {
     public static void main(String[] args) {
 
-        Shape square=new Square();
-        System.out.println("Area of Square = "+square.calculateArea(10,15));
+        Shape square = new Square();
+        System.out.println("Area of Square = " + square.calculateArea(10, 15));
+        //  square.displayArea(10,20);
 
-        Shape rectangle=new Rectangle();
-        System.out.println("Area of Rectangle = "+rectangle.calculateArea(10,30));
+        Shape rectangle = new Rectangle();
+        System.out.println("Area of Rectangle = " + rectangle.calculateArea(10, 30));
 
         Shape triangle = new Triangle();
-        System.out.println("Area of Triangle = "+ triangle.calculateArea(5,10));
+        System.out.println("Area of Triangle = " + triangle.calculateArea(5, 10));
 
-        Shape circle=new Circle();
-        System.out.println("Area of Circle = "+ circle.calculateArea(15,15));
+        Shape circle = new Circle();
+        System.out.println("Area of Circle = " + circle.calculateArea(15, 15));
     }
 }
--- Abstraction/src/Shapes/Rectangle.java
+++ Abstraction/src/Shapes/Rectangle.java
@@ -2,9 +2,21 @@ package Shapes;
 
 import Features.Shape;
 
+/**
+ * This class {@code Rectangle} is an implementation of Shape interface.
+ * This class provides implementations/definition to abstract methods
+ * of the interface {@code Shape}.
+ * By doing so, abstraction is achieved as this class only knows what
+ * methods to override and provide their definitions.
+ */
+
 public class Rectangle implements Shape {
     @Override
     public float calculateArea(int x, int y) {
         return x*y;
+    }
+    @Override
+    public void displayArea(int x,int y){
+        System.out.println(calculateArea(x,y));
     }
 }

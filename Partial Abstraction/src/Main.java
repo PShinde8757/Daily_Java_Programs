500b3dbfcd03681b17ac889e448521263c09d760
Pratik <pratikshinde875@gmail.com>
2023-01-02 08:49:33 +0530

    Update Abstraction Example

--- Partial Abstraction/src/Main.java
+++ Partial Abstraction/src/Main.java
@@ -1,26 +1,20 @@
 import Feature.Shape;
 import Shapes.Circle;
 import Shapes.Rectangle;
-import Shapes.Square;
-import Shapes.Triangle;
 
 public class Main {
     public static void main(String[] args) {
 
-        Shape square=new Square();
-        square.calculateArea(10,10);
-        square.displayArea();
 
         Shape rectangle=new Rectangle();
         rectangle.calculateArea(10,25);
         rectangle.displayArea();
 
-        Shape triangle=new Triangle();
-        triangle.calculateArea(10,5);
-        triangle.displayArea();
 
         Shape circle=new Circle();
         circle.calculateArea(10,10);
         circle.displayArea();
+
+
     }
 }

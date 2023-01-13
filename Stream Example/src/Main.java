10be4e99106bd7a105b56a2aca5b122f84618912
prati <praikshinde8757@gmail.com>
2023-01-14 00:06:41 +0530

    add stream example and lambda expression example

---
+++ Stream Example/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Employee{
+    String name;
+    int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        List<Employee> employeeList= new ArrayList<>();
+
+        employeeList.add(new Employee("Pratik", 10000));
+        employeeList.add(new Employee("Sagar",25000));
+        employeeList.add(new Employee("Khanna",20000));
+        employeeList.add(new Employee("Ashwin",28000));
+        employeeList.add(new Employee("Ankit",230000));
+
+        employeeList.forEach(emp -> System.out.println("Name: "+emp.name+" & "+"Salary: "+emp.salary));
+
+        System.out.println("********************************************************\n");
+
+
+        //Stream --> Sequence of Elements(String, Integer, Double, Student, Employee etc)
+
+        employeeList.stream()
+                .sorted(Comparator.comparing(emp -> emp.name ))
+                .forEach(emp -> System.out.println("Name: "+emp.name+" & "+"Salary: "+emp.salary));
+
+    }
+}
---
+++ lambda expression example/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+@FunctionalInterface
+interface FunctionalTest{
+    int display(int a);
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("********************************************************");
+
+        System.out.println("Print by regular lambda expression\n >");
+        FunctionalTest test =a->a*a*a;
+        System.out.println(test.display(10));
+
+        System.out.println("********************************************************");
+
+        FunctionalTest test1 =a-> {
+                System.out.println("Print by body method lambda expression\n >");
+                return a*a;
+            };
+        System.out.println(test1.display(10));
+
+        System.out.println("********************************************************\n");
+
+        System.out.println("------ Lambda Expression in List Example -----\n");
+
+
+        List<String> guestList =new ArrayList<>();
+        guestList.add("Pratik");
+        guestList.add("Sagar");
+        guestList.add("Khanna");
+        guestList.add("Ashwin");
+        guestList.add("Ankit");
+
+
+        System.out.println("Print all elements of guestList using for loop\n >");
+        for (String guestName: guestList){
+            System.out.println(guestName);
+        }
+
+        System.out.println("********************************************************");
+
+        System.out.println("Print all elements of guestList using Lambda Expression\n >");
+        guestList.forEach(guestName -> System.out.println(guestName));
+
+
+
+    }
+}

bd5004b2b2081b0367bc39d69fa51429de6b384b
Pratik <pratikshinde875@gmail.com>
2023-01-18 09:36:14 +0530

    adding pass by value ,swap example

--- Pass By Value/src/Swap.java
+++ Pass By Value/src/Swap.java
@@ -1,18 +1,17 @@
 public class Swap {
-
-    static void swap(int x,int y){
-        int temp=x;
-        x=y;
-        y=temp;
-
-        System.out.println("before swap");
-        System.out.println(x+" "+y);
-    }
     public static void main(String[] args) {
 
-            int a=10,b=20;
-            swap(a,b);
-        System.out.println("after swap");
+        int a=10,b=20;
+
+        System.out.println("Before swap");
+        System.out.println(a+" "+b);
+
+        int temp=a;
+        a=b;
+        b=temp;
+
+
+        System.out.println("After swap");
         System.out.println(a+" "+b);
 
     }
